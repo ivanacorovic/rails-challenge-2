a8d7acf5021937ff94f05aeddb6f3487acd621f9
ivanacorovic <ivanacorovic55@gmail.com>
2014-02-13 14:36:59 +0100

    final version

--- source/challenge2/spec/factories.rb
+++ source/challenge2/spec/factories.rb
@@ -1,11 +1,10 @@
 FactoryGirl.define do
-  factory :user do
-    sequence(:username)  { |n| "username#{n}" }
-    sequence(:email) { |n| "email#{n}@gmail.com"}
-    sequence(:about)  { |n| "Blah blah Blah" }
-    sequence(:first_name)  { |n| "Person #{n}" }
-    sequence(:last_name) { |n| "person_#{n} last"}
 
+
+  factory :comment do
+    sequence(:comment)  {"Comment tralala"}
+    user
+    story
   end
 
   factory :story do
@@ -15,11 +14,38 @@ FactoryGirl.define do
     sequence(:title) {|n| "Title #{n}"}
     sequence(:description) {"Description"}
     user
+
+    factory :inactive_story do
+      active false
+    end
+
+    factory :story_with_comments do
+      ignore do
+        comment_count 10
+      end
+      after(:create) do |story, evaluator|
+        create_list(:comment, evaluator.comment_count)
+      end
+    end
+
   end
 
-  factory :comment do
-    sequence(:comment)  {"Comment tralala"}
-    user
-    story
-  end
+
+  factory :user do
+    sequence(:username)  { |n| "username#{n}" }
+    sequence(:email) { |n| "email#{n}@gmail.com"}
+    sequence(:about)  { |n| "Blah blah Blah" }
+    sequence(:first_name)  { |n| "Person #{n}" }
+    sequence(:last_name) { |n| "person_#{n} last"}
+
+    factory :user_with_stories do
+      ignore do
+        story_count 5
+      end
+      after(:create) do |user, evaluator|
+        create_list(:story, evaluator.story_count, user: user)
+        create_list(:inactive_story, evaluator.story_count, user: user)
+      end
+    end
+  end 
 end
--- source/challenge2/spec/requests/static_pages_spec.rb
+++ source/challenge2/spec/requests/static_pages_spec.rb
@@ -5,84 +5,44 @@ describe "Static pages" do
   subject {page}
 
   before do
-  	@user1=FactoryGirl.create(:user)
-    @stories=[]
-    10.times do 
-    	@stories << FactoryGirl.create(:story, active: true, user: @user1)
-    end
-    # 10.times do |n|
-    #   @stories << Story.create!(user_id: @user1.id, active: true, url: "https://www.example.org",
-    #                   title: "Story#{n}", description: "This is my Story #{n}")
-    # end
-
-    10.times do
-    	@stories << FactoryGirl.create(:story, active: false, user: @user1)
-    end
-    
-    # 10.times do |n|
-    #   Story.create!(user_id: @user1.id, active: false, url: "https://www.example.org",
-    #                   title: "Story#{n}", description: "This is my Story #{n}")
-    # end
-    @comments=[]
-    35.times do 
-    	@comments << FactoryGirl.create(:comment, user: @user1, story: @stories[3])
-    end
-  	visit user_path(@user1)
+    @user1=FactoryGirl.create(:user_with_stories, story_count: 10)
+    visit user_path(@user1)
   end
-  
-  describe "there should be 3 lists" do
+
+  describe "Activate story" do
+    before do
+      @inactive=Story.where(active: false)
+      @active=Story.where(active: true)
+      check("mycheckbox-#{@inactive.first.id}")
+    end
+    it "test" do
+      expect { click_button "Activate#{@inactive.first.id}"}.to change(@inactive, :count).by(-1)
+    end
+    it "test" do 
+      expect { click_button "Activate#{@inactive.first.id}"}.to change(@active, :count).by(1)
+    end
+  end
+
+  describe "there should be 3 lists: active, inactive and commented." do
     it {should have_selector('table#active')}
     it {should have_selector('table#inactive')}
     it {should have_selector('table#commented')}
   end  
 
-  describe "Correct number of stories" do
+  describe "Correct number of active and inactive stories for User1." do
     it {page.all('table#active tr').count.should eql(11)}
     it {page.all('table#inactive tr').count.should eql(10)}
-    it {page.all('table#commented tr').count.should eql(36)}
-      it "test1" do 
-        @user1.stories.count.should eql (20)
+    
+    describe "Number of stories commented by User1" do
+      before do
+         FactoryGirl.create_list(:comment, 20, user: @user1)
+         visit user_path(@user1)
+      end
+      it {page.all('table#commented tr').count.should eql(21)}
     end
-   
-	end
-   
-  describe "Checking to see if checkbox works" do
-    before do
-    	@inactive=Story.where(active: false)
-    	@active=Story.where(active: true)
-      @stories=Story.all
+  end 	
+    #find(:css, ".mycheckbox-#{@inactive.first.id}").set(true)
     
-      check("mycheckbox-#{@inactive.first.id}")
-      #find(:css, ".mycheckbox-#{@inactive.first.id}").set(true)
-      
-      #find(:css, "#edit_story_#{@inactive.first.id} #story_active").set(true) 
-      #find(:css, "#story_active", :label => "Checkbox#{@inactive.first.id}").set(true) 
-   	end
-    # it "test1" do 
-    #   @inactive.count.should eql (10)
-    # end
-    # it "test1" do 
-    #   @stories.first.id.should eql(@stories.last.id)
-    # end
-    # it "test1" do  
-    #   @active.first.id.should eql(@active.last.id)
-    # end
-    # it "test1" do  
-    #   @stories.count.should eql (20)
-    # end
-    # it "test1" do  
-    #   User.all.count.should eql (1)
-    # end
-    # it "test1" do  
-    #   @inactive.first.id.should eql(@inactive.last.id)
-    # end
-    
- 
-   	it "test" do 
-    	expect { click_button "Activate#{@inactive.first.id}"}.to change(@inactive, :count).by(-1)
-  	end
-		it "test" do 
-    	expect { click_button "Activate#{@inactive.first.id}"}.to change(@active, :count).by(1)
-  	end
-	end
+    #find(:css, "#edit_story_#{@inactive.first.id} #story_active").set(true) 
+    #find(:css, "#story_active", :label => "Checkbox#{@inactive.first.id}").set(true) 	
 end
